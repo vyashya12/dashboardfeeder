05c57bce405d494f30e37a5e417cfb7c0239da78
Vyashya Raj <86505731+vyashya12@users.noreply.github.com>
2023-08-02 12:33:48 +0800

    Update installing-agent.ps1

--- installing-agent.ps1
+++ installing-agent.ps1
@@ -57,7 +57,7 @@ $RESTAPIPassword = $Credentials.GetNetworkCredential().Password
 # Building body to send via http
 $body = @{
     "APIPassword" = "g1paGwcmEYsVlQEKNyfgFwqvj"
-    "ServerName"= "CoolServer"
+    "ServerName"= "CoolServerhahahahha"
     "IP"= "127.0.0.1.00"
     "Drive"= "V"
     "Size"= "30"
