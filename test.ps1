498a4dc245cd06a46669524d21b19b0a2d48cd3d
Vyashya Raj <86505731+vyashya12@users.noreply.github.com>
2023-08-03 16:33:52 +0800

    Test to create scheduled tasks that creates credentials

--- test.ps1
+++ test.ps1
@@ -1,2 +1,4 @@
-cd C:\
-mkdir tests
+Set-Location \
+# Set String
+New-Item -ItemType Directory -Name SecureString
+Get-Credential -Credential (Get-Credential) | Export-Clixml "C:\SecureString\SecureCredentials.xml"
