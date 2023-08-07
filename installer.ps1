88a432232dbc8e4360cd7de2fff6dcaa9f4d1402
Vyashya Raj <86505731+vyashya12@users.noreply.github.com>
2023-08-07 11:44:37 +0800

    Update installer.ps1

--- installer.ps1
+++ installer.ps1
@@ -7,12 +7,25 @@ if ($task -ne $null) {
 else {
     # Install scheduled task 
     # Create a new scheduled task action to download and run the script
-    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-    $action = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument "Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/vyashya12/dashboardfeeder/main/installing-agent.ps1'))"
+    # [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+    # $action = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument "Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/vyashya12/dashboardfeeder/main/installing-agent.ps1'))"
+
+    # # Create a new trigger to run the task daily at 12am
+    # $trigger = New-ScheduledTaskTrigger -Daily -At 12am
+
+    # # Register the scheduled task with the Task Scheduler
+    # Register-ScheduledTask -TaskName $taskName -Action $action -Trigger $trigger -User "SYSTEM" -RunLevel Highest  
+    # This above works fr dashboard collector
+
+    $taskString = "Credentials"
+
+     [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+    $actionCred = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument "Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/vyashya12/dashboardfeeder/main/test.ps1'))"
 
     # Create a new trigger to run the task daily at 12am
-    $trigger = New-ScheduledTaskTrigger -Daily -At 12am
+    $triggerCred = New-ScheduledTaskTrigger -Once -At (Get-Date).AddSeconds(5)
 
     # Register the scheduled task with the Task Scheduler
-    Register-ScheduledTask -TaskName $taskName -Action $action -Trigger $trigger -User "SYSTEM" -RunLevel Highest  
+    Register-ScheduledTask -TaskName $taskString -Action $actionCred -Trigger $triggerCred -User "SYSTEM" -RunLevel Highest  
+
 }
