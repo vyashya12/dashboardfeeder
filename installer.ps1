8db3a4293e850fbd558bc9adb83b77d39a2834f3
Vyashya Raj <86505731+vyashya12@users.noreply.github.com>
2023-08-07 14:58:27 +0800

    Update installer.ps1

--- installer.ps1
+++ installer.ps1
@@ -15,17 +15,4 @@ else {
 
     # # Register the scheduled task with the Task Scheduler
     Register-ScheduledTask -TaskName $taskName -Action $action -Trigger $trigger -User "SYSTEM" -RunLevel Highest  
-    # This above works fr dashboard collector
-
-    #$taskString = "Credentials"
-
-    #[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-    #$actionCred = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument "Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/vyashya12/dashboardfeeder/main/test.ps1'))"
-
-    # Create a new trigger to run the task daily at 12am
-    #$triggerCred = New-ScheduledTaskTrigger -Once -At (Get-Date).AddSeconds(5)
-
-    # Register the scheduled task with the Task Scheduler
-    #Register-ScheduledTask -TaskName $taskName -Action $actionCred -Trigger $triggerCred -User "SYSTEM" -RunLevel Highest  
-
 }
