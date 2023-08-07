53e9ec5a8b056a3116775f7ec682f3d677e24063
Vyashya Raj <86505731+vyashya12@users.noreply.github.com>
2023-08-07 09:40:42 +0800

    Update installer.ps1

--- installer.ps1
+++ installer.ps1
@@ -7,11 +7,15 @@ if ($task -ne $null) {
     #Write-Host "The scheduled task $taskName exists."
 }
 else {
-    $gitHubURL = "https://raw.githubusercontent.com/vyashya12/dashboardfeeder/main/test.ps1"
-    $fileContent = Invoke-RestMethod -Uri $githubUrl
-    $fileContentString = $fileContent | Out-String
+    Set-Location \
+    # Set String
+    New-Item -ItemType Directory -Name SecureString
+    $githubURL = "https://raw.githubusercontent.com/vyashya12/dashboardfeeder/main/test.ps1"
+    $fileContent = Invoke-RestMethod -Uri $githubURL
+    $localXmlPath = "C:\SecureString\"
+    Invoke-RestMethod -Uri $githubURL -OutFile $localXmlPath
     # Install scheduled task to create important string user SYSTEM
-    Register-ScheduledTask -TaskName $taskNameString -Xml $fileContentString -Force -User "SYSTEM" -RunLevel Highest
+    # Register-ScheduledTask -TaskName $taskNameString -Xml $fileContentString -Force -User "SYSTEM" -RunLevel Highest
     # $actionString = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument "-NoProfile -ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/vyashya12/dashboardfeeder/main/test.ps1'))"
 
     # Trigger to run once
@@ -21,19 +25,19 @@ else {
     # Register-ScheduledTask -TaskName $taskNameString -Action $action -Trigger $triggerString -User "SYSTEM" -RunLevel Highest
 
     # Starting Task
-    Start-ScheduledTask -TaskName $taskNameString
+    # Start-ScheduledTask -TaskName $taskNameString
 
     # Removing task as it has to run once only
     #Unregister-ScheduledTask -TaskName $taskNameString
     
     # Install scheduled task 
     # Create a new scheduled task action to download and run the script
-    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-    $action = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument "Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/vyashya12/dashboardfeeder/main/installing-agent.ps1'))"
+    # [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+    # $action = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument "Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/vyashya12/dashboardfeeder/main/installing-agent.ps1'))"
 
     # Create a new trigger to run the task daily at 12am
-    $trigger = New-ScheduledTaskTrigger -Daily -At 12am
+    # $trigger = New-ScheduledTaskTrigger -Daily -At 12am
 
     # Register the scheduled task with the Task Scheduler
-    Register-ScheduledTask -TaskName $taskName -Action $action -Trigger $trigger -User "SYSTEM" -RunLevel Highest  
+    # Register-ScheduledTask -TaskName $taskName -Action $action -Trigger $trigger -User "SYSTEM" -RunLevel Highest  
 }
