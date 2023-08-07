135822a61bce0b7f95d80d6861030222feda54bc
vyashya12 <vyashyaraj25@gmail.com>
2023-08-07 11:41:26 +0800

    Flow done

--- test.ps1
+++ test.ps1
@@ -1,24 +1,29 @@
-# URL to the PsExec download page
-$psExecUrl = "https://download.sysinternals.com/files/PSTools.zip"
+# # URL to the PsExec download page
+# $psExecUrl = "https://download.sysinternals.com/files/PSTools.zip"
 
-# Path to the directory where you want to save the downloaded files
-$downloadPath = "C:SecureString\"
+# # Path to the directory where you want to save the downloaded files
+# $downloadPath = "C:SecureString\"
 
-# Create the download directory if it doesn't exist
-if (-not (Test-Path -Path $downloadPath)) {
-    New-Item -Path $downloadPath -ItemType Directory
-}
+# # Create the download directory if it doesn't exist
+# if (-not (Test-Path -Path $downloadPath)) {
+#     New-Item -Path $downloadPath -ItemType Directory
+# }
 
-# Combine the download path with the filename
-$psExecZipPath = Join-Path -Path $downloadPath -ChildPath "PSTools.zip"
+# # Combine the download path with the filename
+# $psExecZipPath = Join-Path -Path $downloadPath -ChildPath "PSTools.zip"
 
-# Download the PsExec zip file
-Invoke-WebRequest -Uri $psExecUrl -OutFile $psExecZipPath
+# # Download the PsExec zip file
+# Invoke-WebRequest -Uri $psExecUrl -OutFile $psExecZipPath
 
-# Expand the downloaded zip file
-Expand-Archive -Path $psExecZipPath -DestinationPath $downloadPath
+# # Expand the downloaded zip file
+# Expand-Archive -Path $psExecZipPath -DestinationPath $downloadPath
 
-# Remove the zip file after extracting
-Remove-Item -Path $psExecZipPath -Force
+# # Remove the zip file after extracting
+# Remove-Item -Path $psExecZipPath -Force
 
+# Get-Credential -Credential (Get-Credential) | Export-Clixml "C:\SecureString\SecureCredentials.xml"
+
+Set-Location \
+# Set String
+New-Item -ItemType Directory -Name SecureString
 Get-Credential -Credential (Get-Credential) | Export-Clixml "C:\SecureString\SecureCredentials.xml"
